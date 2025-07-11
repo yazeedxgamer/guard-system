ed95a1236a733090d41deb44723d193987d6b55f
yazeedxgamer <hihihihihi569@gmail.com>
2025-07-11 21:33:06 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,33 +1,34 @@
-// This script runs in the background to receive notifications from Firebase
-
-// Import Firebase SDKs
+// === firebase-messaging-sw.js ===
 importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js');
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCXscXexb0bvKEeJ9QKxnrhlB70F0ej7fs",
-  authDomain: "arkanat-287ff.firebaseapp.com",
-  projectId: "arkanat-287ff",
-  storageBucket: "arkanat-287ff.appspot.com",
-  messagingSenderId: "773019407626",
-  appId: "1:773019407626:web:3b534f6c26c970693b16f3",
-  measurementId: "G-81PYGC42FX"
-};
+firebase.initializeApp({
+    apiKey: "AIzaSyCXscXexb0bvKEeJ9QKxnrhlB70F0ej7fs",
+    authDomain: "arkanat-287ff.firebaseapp.com",
+    projectId: "arkanat-287ff",
+    storageBucket: "arkanat-287ff.appspot.com",
+    messagingSenderId: "773019407626",
+    appId: "1:773019407626:web:3b534f6c26c970693b16f3",
+    measurementId: "G-81PYGC42FX"
+});
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Handle incoming messages when the app is in the background
-messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message ', payload);
+messaging.onBackgroundMessage(payload => {
+    console.log('Received background message', payload);
+    const notificationTitle = payload.notification.title || 'اشعار جديد';
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/icons/icon-192.png',
+        data: {
+            url: payload.fcmOptions?.link || '/'
+        }
+    };
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon-192.png'
-  };
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data.url));
 });
